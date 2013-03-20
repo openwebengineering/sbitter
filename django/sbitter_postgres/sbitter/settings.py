8e4ba96e398436dc6dd71329252b5d85a4ed4740
AJ Bahnken <aj@ajvb.me>
2013-03-20 15:01:28 -0700

    removed settings_local.py from production; changed Postgres information.

--- django/sbitter_postgres/sbitter/settings.py
+++ django/sbitter_postgres/sbitter/settings.py
@@ -16,8 +16,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME':  'sbitter',                      # Or path to database file if using sqlite3.
-        'USER': 'sbitter_user',                      # Not used with sqlite3.
+        'NAME':  'sbitterpy',                      # Or path to database file if using sqlite3.
+        'USER': 'sbitter_user_py',                      # Not used with sqlite3.
         'PASSWORD': 'zWUc5ask2DVo7XQmL8q6XEst1g0BgU',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
